94dc106172f88c4cbfc83be353b3dcad3e613558
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-02-03 07:43:06 +0800

    initial push

---
+++ HRMS/Core/Entities/AttendanceSetup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class AttendanceSetup : BaseEntity
+    {
+        public string TimeInType { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ HRMS/HRMS/Form1.cs
@@ -0,0 +1,10 @@
+namespace HRMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
